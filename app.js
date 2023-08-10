4a852c1d0f33e263a46cc31281474705dbaa1fc8
root <vaddemanisiddharth@gmail.com>
2023-08-10 17:22:34 +0000

    committed Code

---
+++ app.js
@@ -0,0 +1,11 @@
+let express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Express JS");
+});
+
+// app.listen(3000);
+
+module.exports = app;
